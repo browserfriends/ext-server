7537b6d785e6e24b11f9a81a84b876612e85de94
Mariya Gedrich <maryged@gmail.com>
2019-03-16 17:55:53 +0000

    location display

--- app.py
+++ app.py
@@ -16,10 +16,21 @@ def fetch():
     # return json!
     return "down"
 
+@application.route("/api/up/loc", methods=['POST'])
+def location():
+    if request.method == "POST":
+        id = request.form['id']
+        loc = request.form['location']
+        id_loc[id] = loc
+    else:
+        return "Invalid loc request"
+    return "loc!"
+
 @application.route("/api/up/open", methods=['POST'])
 def open():
     # add new id/domain pair to the map
     if request.method == "POST":
+        print(request.form)
         id = request.form['id']
         domain = request.form['url']
         id_domain[id] = "{}.{}".format(domain, "open")
