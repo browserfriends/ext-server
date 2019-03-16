3cb9e160c2b37de0e52bb00d079b85ea54500326
b3x0b <b3x0b@ugrad.cs.ubc.ca>
2019-03-16 20:05:00 +0000

    try some timeout

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import json
 import random
+import time
 
 application = Flask(__name__)
 
@@ -10,7 +11,21 @@ id_domain = {} #stores last domain user had open domain:open?
 id_loc = {}
 # domain mapped to list of ids that are there
 domain_id = {}
+# ids to last connection time
+id_time = {}
 curr_id = 0
+
+
+def clean_clients():
+    victims = [i for (i, t) in id_time.items() if t + 15 < time.time()]
+    for v in victims:
+        del id_time[v]
+        if id in id_domain:
+            del id_domain[v]
+
+    for key, value in domain_id.items():
+        domain_id[key] = [i for i in value if i not in victims]
+
 
 def gen_id():
     global curr_id
@@ -31,6 +46,8 @@ def fetch():
 @application.route("/api/down/control", methods=['GET'])
 def command():
     # return json!
+    id_time[request.args.get('id')] = time.time()
+    clean_clients()
     if random.randint(0, 10) > 5:
         return json.dumps({'type': 'notify', 'title': 'This is the server.', 'content': "You are client " + request.args.get('id')})
     else:
@@ -79,6 +96,7 @@ def close():
 
 @application.route("/view-stats")
 def view():
+    clean_clients()
     return render_template("debug.html", id_domain=id_domain, id_loc=id_loc, domain_id=domain_id)
 
 if __name__ == "__main__":
