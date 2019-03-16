280ff56443cc2046ce8fde2c8432938eb2125211
Mariya Gedrich <maryged@gmail.com>
2019-03-16 16:01:32 +0000

    got some routes!

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 
 application = Flask(__name__)
 
@@ -7,6 +8,20 @@ application = Flask(__name__)
 def home():
     return "Welcome to Browserfriends Server!"
 
+@application.route("/api/down/<id>", methods=['GET'])
+def fetch(id):
+    return "down"
+
+@application.route("/api/up/open", methods=['POST'])
+def open():
+    # add to db
+    return "open!"
+
+@application.route("/api/up/close", methods=['POST'])
+    # add to db
+    return "close!"
+
+@application.route()
 if __name__ == "__main__":
     application.run(debug=True)
     application.run(host='0.0.0.0')
